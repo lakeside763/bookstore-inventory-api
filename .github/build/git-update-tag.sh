e45c3c89ad621b997409fe65a92aec7d2ec1cc39
lakeside763 <lakeside763@gmail.com>
2024-01-20 08:56:25 +0100

    chore: add bump npm version

--- .github/build/git-update-tag.sh
+++ .github/build/git-update-tag.sh
@@ -50,9 +50,8 @@ echo "New tag without 'v' - $NEW_TAG_WITH_NO_V"
 git tag $NEW_TAG
 git push origin $NEW_TAG
 
-
-# # Commit and push package.json changes
-# git add package.json
-# npm version $INCREMENT_TYPE -m "chore: Update version to $NEW_TAG_WITH_NO_V"
-# # git commit -m "chore: Update version to $NEW_TAG_WITH_NO_V"
-# git push origin ${GITHUB_REF}
+# Commit and push package.json changes
+git add package.json
+npm version $INCREMENT_TYPE -m "chore: Update version to $NEW_TAG_WITH_NO_V"
+# git commit -m "chore: Update version to $NEW_TAG_WITH_NO_V"
+git push origin ${GITHUB_REF}
