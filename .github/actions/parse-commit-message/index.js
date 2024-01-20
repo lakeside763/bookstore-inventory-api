196e238da277b9764b54caad8a779a88f380599a
lakeside763 <lakeside763@gmail.com>
2024-01-20 02:21:59 +0100

    chore: update changes

--- .github/actions/parse-commit-message/index.js
+++ .github/actions/parse-commit-message/index.js
@@ -3,18 +3,17 @@ const github = require('@actions/github');
 
 try {
   const { pattern } = core.getInput();
-  console.log(pattern)
 
   const commitMessage = github.context.payload.head_commit.message;
   console.log(commitMessage);
-  const match = commitMessage.match(pattern);
-  console.log(match);
+  core.debug('Commit message', commitMessage)
+  // const match = commitMessage.match(pattern);
 
-  if (match) {
-    core.setOutput('extracted_value', match[1]); // Assuming the desired value is in the first capture group
-  } else {
-    core.setFailed(`No match found for pattern: ${pattern}`);
-  }
+  // if (match) {
+  //   core.setOutput('extracted_value', match[1]); // Assuming the desired value is in the first capture group
+  // } else {
+  //   core.setFailed(`No match found for pattern: ${pattern}`);
+  // }
 } catch (error) {
   core.setFailed(error.message);
 }
