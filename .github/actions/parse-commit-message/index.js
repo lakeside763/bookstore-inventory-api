b9cbc83cef1a3075a8ac033f9aaddec0ab4dded8
lakeside763 <lakeside763@gmail.com>
2024-01-20 02:52:37 +0100

    feat!: setup image tag

--- .github/actions/parse-commit-message/index.js
+++ .github/actions/parse-commit-message/index.js
@@ -4,7 +4,18 @@ const github = require('@actions/github');
 try {
   const { pattern } = core.getInput();
 
+  console.log('Pattern:', pattern);
+  if (!pattern) {
+    core.setFailed('Pattern is undefined');
+    return;
+  }
+
   const commitMessage = github.context.payload.head_commit.message;
+  console.log('Commit Message:', commitMessage);
+  if (!commitMessage) {
+    core.setFailed('Commit message is undefined');
+    return;
+  }
   const match = commitMessage.match(pattern);
 
   if (match) {
