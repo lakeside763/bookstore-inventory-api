42b242303734b2e7aa92a33f3e27956bf32ba768
lakeside763 <lakeside763@gmail.com>
2024-01-20 08:41:18 +0100

    chore: update the current tag

--- .github/build/git-update-tag.sh
+++ .github/build/git-update-tag.sh
@@ -5,18 +5,19 @@ git fetch --tags
 
 # Get the latest tag after fetching
 CURRENT_TAG=$(git describe --tags --abbrev=0)
-echo "tag from git - $CURRENT_TAG"
+echo "Tag from git - $CURRENT_TAG"
+
+# If no tags exist, create an initial tag
 if [[ -z "$CURRENT_TAG" ]]; then
-  CURRENT_TAG="v0.0.0"  # Create initial tag if none exists
+  CURRENT_TAG="v0.0.0"
   git tag $CURRENT_TAG
 fi
 
+# Extract version components and cast to numbers
 TAG_VERSION="${CURRENT_TAG#v}"
+IFS='.' read -r CURRENT_MAJOR CURRENT_MINOR CURRENT_PATCH <<< "$TAG_VERSION"
 
-# # Split the version string into components using a period as the delimiter
-IFS=. read -r CURRENT_MAJOR CURRENT_MINOR CURRENT_PATCH <<< "$TAG_VERSION"
-
-# Determine increment type based on extracted value
+# Determine increment type based on input or default to "patch"
 INCREMENT_TYPE="${1:-patch}"
 
 # Increment version components accordingly
@@ -40,10 +41,10 @@ NEW_TAG_WITH_NO_V="${NEW_MAJOR}.${NEW_MINOR}.${NEW_PATCH}"
 
 echo ::set-output name=git-tag::$NEW_TAG
 
-echo "current tag - $CURRENT_TAG"
-echo "INCREMENT TYPE - $INCREMENT_TYPE"
-echo "NEW tag - $NEW_TAG"
-echo "NEW tag WITH NO V - $NEW_TAG_WITH_NO_V"
+echo "Current tag - $CURRENT_TAG"
+echo "Increment type - $INCREMENT_TYPE"
+echo "New tag - $NEW_TAG"
+echo "New tag without 'v' - $NEW_TAG_WITH_NO_V"
 
 # Create and push new tag
 git tag $NEW_TAG
@@ -51,6 +52,6 @@ git push origin $NEW_TAG
 
 # Commit and push package.json changes
 git add package.json
-npm version ${INCREMENT_TYPE} -m "$NEW_TAG_WITH_NO_V"
-git commit -m "chore: Update version to $NEW_TAG_WITH_NO_V"
+npm version $INCREMENT_TYPE -m "chore: Update version to $NEW_TAG_WITH_NO_V"
+# git commit -m "chore: Update version to $NEW_TAG_WITH_NO_V"
 git push origin ${GITHUB_REF}
